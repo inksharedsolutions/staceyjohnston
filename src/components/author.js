688413bc1a6a1410ab01bc6d37cacb68155b1685
inksharedsolutions <webdev.inkshared@gmail.com>
2021-05-17 11:10:08 +0800

    added au photo, updated au content

--- src/components/author.js
+++ src/components/author.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import AuthorImg from '../../static/author/dummy.png'
+import AuthorImg from '../../static/author/author.jpg'
 
 const Author = () => {
     return (
@@ -11,8 +11,7 @@ const Author = () => {
                     <div className="grid-child" id="author-contents">
 
                         <p data-aos="fade-up" data-aos-duration="2000">
-                            Stacey Johnston was raised in Perth, Western Australia. She is a wife and mum to four children.
-
+                            Stacey become a writer, so she could share the stories her vivid imagination creates with readers who are willing to give a seemingly unknown author a chance.
                         </p>
 
                         <h1 className="author-name-tag" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import Nav from '../components/nav'
 import Banner from '../components/non-front-banner'
-import AuthorImg from '../../static/author/dummy.png'
+import AuthorImg from '../../static/author/author.jpg'
 
 const ATB = (props) => {
 
@@ -25,7 +25,7 @@ const ATB = (props) => {
 
                             <div className="heading-quote">
                                 <h4>
-
+                                    If I were normal ... life would be boring
                                 </h4>
                                 <span className="ata-span-fx">
                                 </span>
@@ -35,10 +35,32 @@ const ATB = (props) => {
 
                         <article className="article-section column" id="author">
                             <p>
-                            Stacey Johnston was raised in Perth, Western Australia. She is a wife and mum to four children.
+                                Stacey become a writer, so she could share the stories her vivid imagination creates with readers who are willing to give a seemingly unknown author a chance.
                             </p>
-                            <p>Having been an avid reader most of her life, Stacey finds herself most at ease when she can lose herself in stories of romance and mystery.</p>
-                            <p>Stacey has a creative way of bringing suspense into her stories which keeps her reader hanging onto every word to find out how it ends.</p>
+                            <p>
+                                Currently Stacey resides in an extremely dry and hot mining town located in northern Western Australia. With her husband alongside her, she spends her days tinting paint and serving customers in a paint store.
+                            </p>
+                            <p>
+                                Story telling has always been a big part of who Stacey is and it was her love of telling stories that saw her transfer the visions she sees in her mind onto paper. Disappearing into another world and imagining she is there whilst reading or writing excites her and is something she adores sharing with others.
+                            </p>
+                            <p>
+                                If it’s a particular genre you’re seeking, Stacey may have something that will catch your eye.
+                            </p>
+                            <p>
+                                After publishing her debut novel in 2016 – a romantic suspense story full of intrigue and mystery, which developed into a series; she went on to also publish a romantic comedy based on an Aussie author attending her first author signing in the United States.
+                            </p>
+                            <p>
+                                So not to limit herself, and to aid in coping with a family member who was struggling with Depression and Anxiety, Stacey has also created a beautiful story called After the Rain.
+                            </p>
+                            <p>
+                                Out of all the books Stacey has written this one is her favourite as it tells the story of a man struggling with his mental health issues and the impact it was having on his family.
+                            </p>
+                            <p>
+                                The important addition of surprise, mystery and intrigue she adds to each of her books leaves her readers hanging on to every word, wanting more. 
+                            </p>
+                            <p>
+                                The moto ‘If I were normal, life would be boring’ is not only something Stacey lives by in her everyday life, but also something she brings to her books. Creativity, and a vivid imagination, has helped her shaped many stories that she hopes you love as much as she does. 
+                            </p>
 
                             <p>
                                 <span className="author-name">
